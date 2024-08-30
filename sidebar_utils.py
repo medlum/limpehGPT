68f1ccd425253dd9a42b693fd3f803b10c4ae111
Andy Oh <52624830+medlum@users.noreply.github.com>
2024-08-30 16:27:55 +0800

    Update sidebar_utils.py

--- sidebar_utils.py
+++ sidebar_utils.py
@@ -16,8 +16,7 @@ commit_message = 'Update CSV file'
 github = Github(token)
 repo = github.get_user(repo_owner).get_repo(repo_name)
 github_url = f'https://raw.githubusercontent.com/{repo_owner}/{repo_name}/main/{github_file_path}'
-response = requests.get(github_url)
-df = pd.read_csv(StringIO(response.text))
+
 
 # ------setup schedule widgets -------#
 
@@ -63,6 +62,9 @@ def schedule_widgets():
     # if confirm_button:
     if btn_schedule == "Pen it":
 
+        response = requests.get(github_url)
+        df = pd.read_csv(StringIO(response.text))
+
         start_date = select_date[0]
 
         if len(select_date) == 2:
@@ -97,21 +99,3 @@ def schedule_widgets():
         response = requests.get(github_url)
         st.write(pd.read_csv(StringIO(response.text)))
 
-        # with st.container(height=230):
-        #    st.write("Date : ", select_date)
-        #    st.write("From : ", start_time, "To : ", end_time)
-        #    st.write("Type : ",  appt_type)
-        #    st.write("Title : ", appt_type)
-        #    st.write("Location : ", location)
-
-        # https://stackoverflow.com/questions/76238677/how-to-programmatically-read-and-update-a-csv-file-stored-in-a-github-repo
-        # write data to calendar.csv
-
-        #        data_path = Path.cwd()/"data/calendar.csv"
-        #        if data_path.exists():
-        #            with data_path.open(mode='a', encoding="UTF-8", newline="") as write_file:
-        #                writer = csv.writer(write_file)
-        #                # writer.writerow(["date", "start_time",
-        #                #                "end_time", "select_type", "title", "location"])
-        #                writer.writerow(
-        #                    [select_date, start_time, end_time, select_type, title, location]
