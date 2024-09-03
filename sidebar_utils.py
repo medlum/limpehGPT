8125275a9bf66cecb9ddcdc749aaa1300e17ce1f
Andy Oh <52624830+medlum@users.noreply.github.com>
2024-09-03 14:14:19 +0800

    Update sidebar_utils.py

--- sidebar_utils.py
+++ sidebar_utils.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import datetime
 import streamlit as st
@@ -20,113 +19,103 @@ github = Github(token)
 
 def schedule_widgets():
 
-    st.subheader(":blue[Appointment Pad]")
+    # setup date time
+    today = datetime.datetime.today()
+    one_day = datetime.timedelta(days=2)
+    next = today + one_day
+    one_day = datetime.timedelta(days=1)
 
-    with st.container(height=360):
+    st.subheader(":blue[Schedule]")
 
-        appt_type = st.selectbox(":blue[Type]", options=["Work",
-                                                         "Friends",
-                                                         "Family",
-                                                         "Errand",
-                                                         "Medical",
-                                                         "Birthday",
-                                                         "Anniversary",
-                                                         "Special Event",
-                                                         "Holiday"])
+    with st.form("schedule_form", clear_on_submit=True, border=True):
+
+        appt_type = st.selectbox(":blue[Type]",
+                                 options=["Please select",
+                                          "Work",
+                                          "Friends",
+                                          "Family",
+                                          "Personal Errand",
+                                          "Medical",
+                                          "Birthday Reminder",
+                                          "Anniversary",
+                                          "Special Event",
+                                          "Holiday"])
 
         appt_title = st.text_input(":blue[Title]")
 
         location = st.text_input(":blue[Location]")
 
-        #  Columns for date, and time entry
-        date_col, start_time_col, end_time_col = st.columns([1, 1, 1])
+        # create 3 columns for date and time entry
+        date_col, start_time_col, end_time_col = st.columns([2, 1, 1])
+
         # col 1
-        with date_col:
-            today = datetime.datetime.today()
-            one_day = datetime.timedelta(days=2)
-            next = today + one_day
-            one_day = datetime.timedelta(days=1)
-
-            select_date = st.date_input(
-                ":blue[Date]",  (today.date(), next.date()))
-
-            # extract start and end date
-            start_date = select_date[0]
-            if len(select_date) == 2:
-                end_date = str(select_date[1])
-            else:
-                end_date = str(select_date[0])
+        select_date = date_col.date_input(":blue[Date]",
+                                          (today.date(),
+                                           next.date()))
 
         # col 2 start time
-        with start_time_col:
-            start_time = st.time_input(
-                ":blue[Start time]", datetime.time(00, 00), key='start')
+        start_time = start_time_col.time_input(":blue[Start time]",
+                                               datetime.time(00, 00),
+                                               key='start')
 
         # col 3 end time
-        with end_time_col:
-            end_time = st.time_input(
-                ":blue[End time]", datetime.time(00, 00), key='end')
+        end_time = end_time_col.time_input(":blue[End time]",
+                                           datetime.time(00, 00),
+                                           key='end')
 
-    # confirm_button = st.button(":blue[Pen it]")
-    btn_schedule = schedule_buttons()
+        # extract start and end date
+        start_date = select_date[0]
+        if len(select_date) == 2:
+            end_date = str(select_date[1])
+        else:
+            end_date = str(select_date[0])
 
-    # if confirm_button:
-    if btn_schedule == "Pen schedule":
-        # df.to_csv('temp_data.csv', index=False)
+        submitted = st.form_submit_button("Submit")
 
-        repo = github.get_user(repo_owner).get_repo(repo_name)
-        github_url = f'https://raw.githubusercontent.com/{repo_owner}/{repo_name}/main/{github_file_path}'
-        response = requests.get(github_url)
-        df = pd.read_csv(StringIO(response.text))
+        if submitted:
+            # remove YYYY from date entry as annual reminder
+            if appt_type.lower() == "birthday reminder":
+                start_date = str(start_date)[5:]
+                end_date = str(end_date)[5:]
 
-        # insert to last row of df from github
-        df.loc[len(df.index)] = [start_date,
-                                 end_date,
-                                 start_time,
-                                 end_time,
-                                 appt_type,
-                                 appt_title,
-                                 location]
-        # convert to csv
-        contents = df.to_csv(index=False)
-        # fetch calendar.csv  from github
-        content = repo.get_contents(github_file_path)
+            repo = github.get_user(repo_owner).get_repo(repo_name)
+            github_url = f'https://raw.githubusercontent.com/{repo_owner}/{repo_name}/main/{github_file_path}'
+            response = requests.get(github_url)
+            df = pd.read_csv(StringIO(response.text))
 
-        # with open('temp_data.csv', 'rb') as f:
-        #    contents = f.read()
+            # insert to last row of df from github
+            df.loc[len(df.index)] = [start_date,
+                                     end_date,
+                                     start_time,
+                                     end_time,
+                                     appt_type,
+                                     appt_title,
+                                     location]
+            # convert to csv
+            contents = df.to_csv(index=False)
+            # fetch calendar.csv  from github
+            content = repo.get_contents(github_file_path)
 
-        # update github calendar.csv
-        repo.update_file(github_file_path,
-                         commit_message,
-                         contents,
-                         content.sha)
+            # update github calendar.csv
+            repo.update_file(github_file_path,
+                             commit_message,
+                             contents,
+                             content.sha)
 
-        st.toast('Pinned in caldendar.', icon="✅")
+            st.toast('Pinned in caldendar.', icon="✅")
 
-    # view_button = st.button(":blue[View it]")
-    # if view_button:
-    if btn_schedule == "View schedule":
+    if sac.buttons([sac.ButtonsItem(icon=sac.BsIcon(name='table', size=15),
+                                    label="View schedule", )],
+                   align='left',
+                   size="sm",
+                   index=2,
+                   gap='md',
+                   radius='md'):
 
         repo = github.get_user(repo_owner).get_repo(repo_name)
         github_url = f'https://raw.githubusercontent.com/{repo_owner}/{repo_name}/main/{github_file_path}'
         response = requests.get(github_url)
         st.write(pd.read_csv(StringIO(response.text)))
 
-        # with st.container(height=230):
-        #    st.write("Date : ", select_date)
-        #    st.write("From : ", start_time, "To : ", end_time)
-        #    st.write("Type : ",  appt_type)
-        #    st.write("Title : ", appt_type)
-        #    st.write("Location : ", location)
 
-        # https://stackoverflow.com/questions/76238677/how-to-programmatically-read-and-update-a-csv-file-stored-in-a-github-repo
-        # write data to calendar.csv
-
-        #        data_path = Path.cwd()/"data/calendar.csv"
-        #        if data_path.exists():
-        #            with data_path.open(mode='a', encoding="UTF-8", newline="") as write_file:
-        #                writer = csv.writer(write_file)
-        #                # writer.writerow(["date", "start_time",
-        #                #                "end_time", "select_type", "title", "location"])
-        #                writer.writerow(
-        #                    [select_date, start_time, end_time, select_type, title, location]
+# https://stackoverflow.com/questions/76238677/how-to-programmatically-read-and-update-a-csv-file-stored-in-a-github-repo
